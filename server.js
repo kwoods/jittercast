93ba18af820551b6529bafd6747cfa23cc36529d
Kevin Woods <kevin@tcnetwork.tv>
2014-04-07 16:45:41 -0400

    testing div resize and media player

--- server.js
+++ server.js
@@ -6,16 +6,6 @@ var server = http.createServer();
  
 
 
-var msg = "the cake is a lie!";
-
-server.on('request', function(req, res){
-  if(req.method == "POST" && req.url == "/") {
-    msg = "the cake is a lie!";
-  }
-  res.writeHead(204);
-  res.end();
-}).listen(env.node_port, "0.0.0.0");
-
 io.sockets.on('connection', function(sock) {
   console.log('new client connected');
 
@@ -24,13 +14,18 @@ io.sockets.on('connection', function(sock) {
   });
 
   sock.on('huge success', function(){
-    msg = '<style type="text/css" media="screen"> .as_widget_sidebar { width:486px; height:805px; position: absolute; top: 138px; left: 1434px; background-color: purple; border: none; } </style><div id="as_widget_sidebar" class="as_widget_sidebar"></div>';
+    msg = '<style type="text/css" media="screen"> .as_widget_sidebar { overflow: hidden; width:486px; height:805px; position: absolute; top: 138px; left: 1434px;  border: 1px solid #000; } </style><div id="as_widget_sidebar" class="as_widget_sidebar"></div>';
     io.sockets.emit('content', {message: msg});
   });
   
-  sock.on('sidebar', function(){
+  sock.on('sidebar_style', function(){
+    msg = '"width":"1920px","height":"805px","top":"0px","left":"0px"';
+    io.sockets.emit('sidebar_style', {message: msg});
+  });
+
+  sock.on('sidebar_load', function(){
     msg = '';
-    io.sockets.emit('test', {message: msg});
+    io.sockets.emit('sidebar_load', {message: msg});
   });
     
 });
